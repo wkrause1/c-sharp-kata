02848cd82581da40d163b47dc06bdb4344fece1b
Will Krause <will@Wills-MacBook-Pro.local>
2019-09-06 12:57:13 -0400

    Initial commit

---
+++ Test/Game.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Test
+{
+    public static class Game
+    {
+       
+        public static bool isMagicSquare(List<List<decimal>> matrix)
+        {
+            return false;
+        }
+    }
+}
---
+++ Test/Test.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+namespace Test
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void When_MatrixIsLessThanThreeByThree_Then_IsMagicSquare_False() 
+        {
+            var matrix = new List<List<decimal>>
+            {
+                new List<decimal> {1.0m, 2.0m},
+                new List<decimal> {3.0m, 4.0m }
+            };
+            Assert.IsFalse(Game.isMagicSquare(matrix));
+        }
+
+
+    }
+}
